0f6b2c80b364e03ef92d15bd4090a3a5eb0f6ea7
Jesse Wagner <63376650+BreakNeckJim@users.noreply.github.com>
2022-07-15 14:14:23 -0300

    Update password_generator.py
    Finished password generator

--- password_generator.py
+++ password_generator.py
@@ -1,4 +1,13 @@
+import random
+
 lowercase = "abcdefghijklmnopqrstuvwxyz"
 uppercase = lowercase.upper()
 numbers = "0123456789"
 symbols = "~!@#$%^&*()_+"
+
+ans = lowercase + uppercase + numbers + symbols
+
+length = 10
+password = "".join(random.sample(ans, length))
+
+print("Generated password is: ", password)
